3e1495e0e75e49048be3f206f6bc2eb07f985439
Sergio Fernandez <s.fernandez01@gmail.com>
2025-06-19 21:52:05 -0400

    test yml init python

--- app/api.py
+++ app/api.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify
-
-
 from app.cupones import calcular_precio_final
+
 app = Flask(__name__)
+
 @app.route('/precio', methods=['POST'])
 def calcular():
     data = request.get_json()
     precio = data.get("precio")
     cupon = data.get("cupon")
     impuesto = data.get("impuesto", 0.19)
+
     final = calcular_precio_final(precio, cupon, impuesto)
     return jsonify({"precio_final": final})
